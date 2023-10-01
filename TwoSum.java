e288c9b16c54a1995a9844e109171bf7d4aa6d33
Yash Ghugardare <117991996+yghugardare@users.noreply.github.com>
2023-10-01 18:21:21 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -13,7 +13,23 @@ public class TwoSum {
         }
         return ans;
     }
+    static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer>hm = new HashMap<Integer,Integer>();
+        int[] ans = new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])){
+                ans[0] = hm.get(target-nums[i]);
+                ans[1] = i;
+            }
+            
+            hm.put(nums[i],i);
+
+        }
+        return ans;
+    }
     public static void main(String[] args) {
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
         
     }
 }
